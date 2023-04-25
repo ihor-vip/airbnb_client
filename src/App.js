eb1e4e4543204c6c4ae6716eda982af04f97a228
ihor-vip <vip20140902@gmail.com>
2023-04-25 13:00:50 +0300

    Account page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import LoginPage from "./pages/LoginPage";
 import Layout from "./components/Layout";
 import RegisterPage from "./pages/RegisterPage";
 import {UserContextProvider} from "./UserContext";
+import AccountPage from "./pages/AccountPage";
 
 axios.defaults.baseURL = 'http://localhost:8080';
 axios.defaults.withCredentials = true;
@@ -17,6 +18,7 @@ function App() {
                   <Route index element={<IndexPage />} />
                   <Route path='/login' element={<LoginPage />} />
                   <Route path='/register' element={<RegisterPage />} />
+                  <Route path='/account/:subpage?' element={<AccountPage />} />
               </Route>
           </Routes>
       </UserContextProvider>
