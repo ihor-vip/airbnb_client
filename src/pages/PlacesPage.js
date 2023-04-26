20a3474191f6d7e7aef5dfb3b7f313876690fb3b
ihor-vip <vip20140902@gmail.com>
2023-04-26 09:12:22 +0300

    Upload photo by link

--- src/pages/PlacesPage.js
+++ src/pages/PlacesPage.js
@@ -1,7 +1,51 @@
 import {Link, useParams} from "react-router-dom";
+import {useState} from "react";
+import Perks from "../components/Perks";
+import axios from "axios";
 
 export default function PlacePage() {
     const {action} = useParams();
+    const [title, setTitle] = useState('');
+    const [address, setAddress] = useState('');
+    const [addedPhotos, setAddedPhotos] = useState([]);
+    const [photoLink, setPhotoLink] = useState('');
+    const [description, setDescription] = useState('');
+    const [perks, setPerks] = useState([]);
+    const [extraInfo, setExtraInfo] = useState('');
+    const [checkIn, setCheckIn] = useState('');
+    const [checkOut, setCheckOut] = useState('');
+    const [maxGuests, setMaxGuests] = useState(1);
+
+    function inputHeader(text) {
+        return (
+            <h2 className="text-2xl mt-4">{text}</h2>
+        )
+    }
+
+    function inputDescription(text) {
+        return (
+            <p className="text-gray-500 text-sm">{text}</p>
+        )
+    }
+
+    function preInput(header, description) {
+        return (
+            <>
+                {inputHeader(header)}
+                {inputDescription(description)}
+            </>
+        )
+    }
+
+    async function addPhotoByLInk(e) {
+        e.preventDefault();
+        const {data:filename} = await axios.post('/upload-by-link', {link: photoLink});
+        setAddedPhotos(prev => {
+            return [...prev, filename]
+        });
+        setPhotoLink('');
+    }
+
     return (
         <div>
             {action !== 'new' && (
@@ -17,16 +61,63 @@ export default function PlacePage() {
             {action === 'new' && (
                <div>
                    <form>
-                       <h2 className="text-2xl mt-4">Title</h2>
-                       <p className="text-gray-500 text-sm">Title for your place. Should be short and catchy as in advertisement</p>
-                       <input type="text" placeholder="title, for example: My lovely apartments" />
-                       <h2 className="text-xl mt-4">Address</h2>
-                       <p className="text-gray-500 text-sm">Address to this place.</p>
-                       <input type="text" placeholder="address" />
-                       <h2 className="text-xl mt-4">Photos</h2>
-                       <p className="text-gray-500 text-sm">more = better</p>
-                       <div className="mt-2 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
-                           <button className="border bg-transparent rounded-2xl p-8 text-2xl text-gray-600">+</button>
+                       {preInput('Title', 'Title for your place. Should be short and catchy as in advertisement')}
+                       <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="title, for example: My lovely apartments" />
+
+                       {preInput('Address', 'Address to this place.')}
+                       <input type="text" value={address} onChange={(e) => setAddress(e.target.value)} placeholder="address" />
+
+                       {preInput('Photos', 'more = better')}
+                       <div className="flex gap-2">
+                           <input type="text" value={photoLink} onChange={(e) => setPhotoLink(e.target.value)} placeholder={'Add using a link ...jpg'} />
+                           <button onClick={addPhotoByLInk} className="bg-gray-200 px-4 rounded-2xl">Add&nbsp;photo</button>
+                       </div>
+
+                       <div className="mt-2 grid gap-2 grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
+                           {addedPhotos.length > 0 && addedPhotos.map(link => (
+                               <div>
+                                   <img className="rounded-2xl" src={'http://localhost:8080/uploads/'+link} alt="photo"/>
+                               </div>
+                           ))}
+                           <button className="flex items-center justify-center gap-1 border bg-transparent rounded-2xl p-2 text-2xl text-gray-600">
+                               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+                                   <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
+                               </svg>
+                               Upload
+                           </button>
+                       </div>
+
+                       {preInput('Description', 'description of the place')}
+                       <textarea value={description} onChange={(e) => setDescription(e.target.value)}/>
+
+                       {preInput('Perks', 'select all the perks of your place')}
+                       <div className="grid mt-2 gap-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-6">
+                           <Perks selected={perks} onChange={setPerks} />
+                       </div>
+
+                       {preInput('Extra info', 'house rules, etc')}
+                       <textarea value={extraInfo} onChange={(e) => setExtraInfo(e.target.value)} />
+
+                       {preInput('Check in&out times', 'add check in and out times, remember to have some time window for cleaning the room between the guests')}
+                       <div className="grid gap-2 sm:grid-cols-3">
+                           <div>
+                               <h3 className="mt-2 -mb-1">Check in time</h3>
+                               <input value={checkIn} onChange={(e) => setCheckIn(e.target.value)} type="text" placeholder="14:00" />
+                           </div>
+
+                           <div>
+                               <h3 className="mt-2 -mb-1">Check out time</h3>
+                               <input value={checkOut} onChange={(e) => setCheckOut(e.target.value)} type="text" placeholder="12:00" />
+                           </div>
+
+                           <div>
+                               <h3 className="mt-2 -mb-1">Max number of guests</h3>
+                               <input value={maxGuests} onChange={(e) => setMaxGuests(e.target.value)} type="number" placeholder="3" />
+                           </div>
+                       </div>
+
+                       <div>
+                           <button className="primary my-4">Save</button>
                        </div>
                    </form>
                </div>
