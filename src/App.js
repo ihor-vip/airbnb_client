f55c74036417742a192e4e5cc18b6c21bb691d6e
ihor-vip <vip20140902@gmail.com>
2023-04-30 11:23:00 +0300

    user booking page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import {Route, Routes} from "react-router-dom";
 import axios from "axios";
+import {UserContextProvider} from "./UserContext";
+import Layout from "./components/Layout";
 import IndexPage from "./pages/IndexPage";
 import LoginPage from "./pages/LoginPage";
-import Layout from "./components/Layout";
 import RegisterPage from "./pages/RegisterPage";
-import {UserContextProvider} from "./UserContext";
 import ProfilePage from "./pages/ProfilePage";
 import PlacesPage from "./pages/PlacesPage";
 import PlacesFormPage from "./pages/PlacesFormPage";
 import PlacePage from "./pages/PlacePage";
+import BookingsPage from "./pages/BookingsPage";
+import BookingPage from "./pages/BookingPage";
 
 axios.defaults.baseURL = 'http://localhost:8080';
 axios.defaults.withCredentials = true;
+
 
 function App() {
     return (
@@ -21,11 +24,13 @@ function App() {
                   <Route index element={<IndexPage />} />
                   <Route path='/login' element={<LoginPage />} />
                   <Route path='/register' element={<RegisterPage />} />
+                  <Route path='/place/:id' element={<PlacePage />} />
                   <Route path='/account' element={<ProfilePage />} />
                   <Route path='/account/places' element={<PlacesPage />} />
                   <Route path='/account/places/new' element={<PlacesFormPage />} />
                   <Route path='/account/places/:id' element={<PlacesFormPage />} />
-                  <Route path='/place/:id' element={<PlacePage />} />
+                  <Route path="/account/bookings" element={<BookingsPage />} />
+                  <Route path="/account/bookings/:id" element={<BookingPage />} />
               </Route>
           </Routes>
       </UserContextProvider>
--- src/pages/PlacesPage.js
+++ src/pages/PlacesPage.js
@@ -2,6 +2,7 @@ import {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
 import axios from "axios";
 import AccountNav from "../components/AccountNav";
+import PlaceImg from "../components/PlaceImg";
 
 export default function PlacePage() {
     const [places, setPlaces] = useState([]);
@@ -26,9 +27,7 @@ export default function PlacePage() {
                         {places.length > 0 && places.map(place => (
                             <Link to={"/place/"+place._id} className="flex cursor-pointer gap-4 bg-gray-100 p-4 rounded-2xl">
                                 <div className="flex w-32 h-32 bg-gray-300 grow shrink-0">
-                                    {place.photos.length > 0 && (
-                                        <img className="object-cover" src={'http://localhost:8080/uploads/'+place.photos[0]} alt=""/>
-                                    )}
+                                   <PlaceImg place={place} />
                                 </div>
                                 <div className="grow-0 shrink">
                                     <h2 className="text-xl">{place.title}</h2>
