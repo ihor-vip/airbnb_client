80603e450e6e66b36c1a4cc55a59d901627a9933
ihor-vip <vip20140902@gmail.com>
2023-05-01 12:32:14 +0300

    S3 AWS Bucket

--- src/components/PlaceGallery.js
+++ src/components/PlaceGallery.js
@@ -1,4 +1,5 @@
 import {useState} from "react";
+import Image from "./Image";
 
 export default function PlaceGallery({place}) {
     const [showAllPhotos, setShowAllPhotos] = useState(false);
@@ -18,7 +19,7 @@ export default function PlaceGallery({place}) {
                     </div>
                     {place?.photos?.length > 0 && place.photos.map(photo => (
                         <div>
-                            <img src={'http://localhost:8080/uploads/'+photo} alt=""/>
+                            <Image src={photo} alt=""/>
                         </div>
                     ))}
                 </div>
@@ -31,19 +32,19 @@ export default function PlaceGallery({place}) {
             <div className="grid gap-2 grid-cols-[2fr_1fr] rounded-3xl overflow-hidden">
                 <div>
                     {place.photos?.[0] && (
-                        <img onClick={() => setShowAllPhotos(true)} className="aspect-square cursor-pointer object-cover" src={'http://localhost:8080/uploads/'+place.photos[0]} alt=""/>
+                        <Image onClick={() => setShowAllPhotos(true)} className="aspect-square cursor-pointer object-cover" src={place.photos[0]} alt=""/>
                     )}
                 </div>
                 <div className="grid">
                     {place.photos?.[1] && (
                         <div>
-                            <img onClick={() => setShowAllPhotos(true)} className="aspect-square cursor-pointer object-cover" src={'http://localhost:8080/uploads/'+place.photos[1]} alt=""/>
+                            <Image onClick={() => setShowAllPhotos(true)} className="aspect-square cursor-pointer object-cover" src={place.photos[1]} alt=""/>
                         </div>
                     )}
 
                     <div className="overflow-hidden">
                         {place.photos?.[2] && (
-                            <img onClick={() => setShowAllPhotos(true)} className="aspect-square cursor-pointer object-cover relative top-2" src={'http://localhost:8080/uploads/'+place.photos[2]} alt=""/>
+                            <Image onClick={() => setShowAllPhotos(true)} className="aspect-square cursor-pointer object-cover relative top-2" src={place.photos[2]} alt=""/>
                         )}
                     </div>
                 </div>
